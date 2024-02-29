4bae21c3539214d8b24db395554f38e6f71897f0
Kay Freyer <kay.freyer@icloud.com>
2024-02-29 12:47:24 +0100

    Ignore .db files

---
+++ .dir-locals.el
@@ -0,0 +1,2 @@
+((nil . ((org-roam-directory . "/home/kay/workspace/note-taking-app/org-roam-files")
+         (org-roam-db-location . "/home/kay/workspace/note-taking-app/org-roam-files/note-taking-app.db"))))
