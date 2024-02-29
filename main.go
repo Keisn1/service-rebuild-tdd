4c902bbdb2cb312f68dcd2dd380f2b13cabc58eb
Kay Freyer <kay.freyer@icloud.com>
2024-02-29 14:49:04 +0100

    Get back []string in json

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func NotesService(w http.ResponseWriter, r *http.Request) {
+	notes := []string{"Note number 1", "Note number 2"}
+	json.NewEncoder(w).Encode(notes)
+}
---
+++ server_test.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"reflect"
+	"testing"
+)
+
+func TestGetNotes(t *testing.T) {
+	t.Run("Server returns all notes", func(t *testing.T) {
+		request, _ := http.NewRequest(http.MethodGet, "/notes", nil)
+		response := httptest.NewRecorder()
+		NotesService(response, request)
+
+		var got []string
+		json.NewDecoder(response.Body).Decode(&got)
+		want := []string{"Note number 1", "Note number 2"}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf(`got = %v; want %v`, got, want)
+		}
+	})
+}
