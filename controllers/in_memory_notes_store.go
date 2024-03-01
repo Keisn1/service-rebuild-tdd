d976dc464a7cf10050abee3922001d52550490cb
Kay Freyer <kay.freyer@icloud.com>
2024-03-01 17:14:22 +0100

    Add lock to In Memory Notes Store

--- controllers/in_memory_notes_store.go
+++ controllers/in_memory_notes_store.go
@@ -1,18 +1,27 @@
 package controllers
 
+import "sync"
+
 type InMemoryNotesStore struct {
 	notes map[int][]string
+	lock  sync.RWMutex
 }
 
 func NewInMemoryNotesStore(data map[int][]string) InMemoryNotesStore {
-	return InMemoryNotesStore{notes: data}
+	return InMemoryNotesStore{notes: data, lock: sync.RWMutex{}}
 }
 
 func (i *InMemoryNotesStore) GetNotesByID(id int) []string {
+	i.lock.Lock()
+	defer i.lock.Unlock()
+
 	return i.notes[id]
 }
 
 func (i *InMemoryNotesStore) GetAllNotes() []string {
+	i.lock.Lock()
+	defer i.lock.Unlock()
+
 	var allNotes []string
 	for _, notes := range i.notes {
 		allNotes = append(allNotes, notes...)
@@ -21,6 +30,8 @@ func (i *InMemoryNotesStore) GetAllNotes() []string {
 }
 
 func (i *InMemoryNotesStore) AddNote(userID int, note string) error {
+	i.lock.Lock()
+	defer i.lock.Unlock()
 	i.notes[userID] = append(i.notes[userID], note)
 	return nil
 }
