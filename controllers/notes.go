decc35d0da12a4321e730c6c1d4223ecc2c9cd2d
Kay Freyer <kay.freyer@icloud.com>
2024-03-01 15:48:49 +0100

    Remove errors that have not been testing for yet
    TDD asks for minimal code, and i realized that i introduced
    assomptions that I have not been reflecting on. In particular
    error handling

--- controllers/notes.go
+++ controllers/notes.go
@@ -2,9 +2,7 @@ package controllers
 
 import (
 	"encoding/json"
-	"fmt"
 	"github.com/go-chi/chi"
-	"log"
 	"net/http"
 	"strconv"
 	"strings"
@@ -21,32 +19,17 @@ type Notes struct {
 }
 
 func (ns *Notes) ProcessAddNote(w http.ResponseWriter, r *http.Request) {
-	userID, err := strconv.Atoi(strings.TrimPrefix(r.URL.Path, "/notes/"))
-	if err != nil {
-		log.Println(fmt.Errorf("NotesServer.ServeHTTP StringConverting: %w", err))
-		http.Error(w, "There was an Error retrieving Notes", http.StatusInternalServerError)
-	}
+	userID, _ := strconv.Atoi(strings.TrimPrefix(r.URL.Path, "/notes/"))
 	var body map[string]string
-	err = json.NewDecoder(r.Body).Decode(&body)
-	if err != nil {
-		log.Println(fmt.Errorf("NotesServer.ServeHTTP, JsonDecoding: %w", err))
-		http.Error(w, "There was an Error retrieving Notes", http.StatusInternalServerError)
-	}
+	_ = json.NewDecoder(r.Body).Decode(&body)
 
 	note := body["note"]
-	err = ns.NotesStore.AddNote(userID, note)
-	if err != nil {
-
-	}
+	_ = ns.NotesStore.AddNote(userID, note)
 	w.WriteHeader(http.StatusAccepted)
 }
 
 func (ns *Notes) GetNotesByID(w http.ResponseWriter, r *http.Request) {
-	userID, err := strconv.Atoi(chi.URLParam(r, "id"))
-	if err != nil {
-		log.Println(fmt.Errorf("NotesServer.ServeHTTP: %w", err))
-		http.Error(w, "There was an Error retrieving Notes", http.StatusInternalServerError)
-	}
+	userID, _ := strconv.Atoi(chi.URLParam(r, "id"))
 	notes := ns.NotesStore.GetNotesByID(userID)
 	if len(notes) == 0 {
 		w.WriteHeader(http.StatusNotFound)
