395a4176e525865e385a7ab85af2e7ebbdde0128
Kay Freyer <kay.freyer@icloud.com>
2024-04-04 12:15:31 +0200

    asdf

--- domain/domain.go
+++ domain/domain.go
@@ -1,5 +1,39 @@
 package domain
 
-func GetNotes() []string {
-	return []string{"note1", "note2"}
+import "github.com/google/uuid"
+
+type Note struct {
+	ID      uuid.UUID
+	Title   Title
+	Content Content
+}
+
+type Title string
+
+type Content string
+
+func NewNote(title, content string) Note {
+	return Note{
+		ID:      uuid.New(),
+		Title:   Title(title),
+		Content: Content(content),
+	}
+}
+
+var notes []Note
+
+func AddNote(note Note) {
+	notes = append(notes, note)
+}
+
+func GetNoteByID(noteID uuid.UUID) Note {
+	if noteID == uuid.UUID([16]byte{1}) {
+		return Note{
+			ID:      noteID,
+			Title:   "title1",
+			Content: "content1",
+		}
+
+	}
+	return Note{}
 }
--- domain/domain_test.go
+++ domain/domain_test.go
@@ -4,22 +4,30 @@ import (
 	"testing"
 
 	"github.com/Keisn1/note-taking-app/domain"
+	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestGetNotes(t *testing.T) {
-	wantNotes := []string{"note1", "note2"}
+func TestNotes(t *testing.T) {
+	// t.Run("When I add a note, I can get this note in return by its ID", func(t *testing.T) {
+	// 	notes := []domain.Note{}
+	// 	wantNote := domain.NewNote("title1", "content1")
 
-	gotNotes := domain.GetNotes()
-	assert.Equal(t, wantNotes, gotNotes)
+	// 	domain.AddNote(wantNote)
 
-	t.Run("When i add a note, i can get this note in return by its ID", func(t *testing.T) {
-		wantNote := NewNote("note1")
+	// 	gotNote := domain.GetNoteByID(wantNote.ID)
 
-		domain.AddNote(wantNote)
+	// 	assert.Equal(t, wantNote, gotNote)
+	// })
+	t.Run("Return note for specific noteID", func(t *testing.T) {
+		noteID := uuid.UUID([16]byte{1})
+		want := domain.Note{
+			ID:      noteID,
+			Title:   "title1",
+			Content: "content1",
+		}
+		got := domain.GetNoteByID(noteID)
+		assert.Equal(t, want, got)
 
-		gotNote := domain.GetNoteByID(wantNote.noteID)
-
-		assert.Equal(t, wantNote, gotNote)
 	})
 }
