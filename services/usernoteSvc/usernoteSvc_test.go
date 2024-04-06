eb15543e882241b9c8731240af3d68214dbc9212
Kay Freyer <kay.freyer@icloud.com>
2024-04-06 11:50:03 +0200

    Red: Test NewNotesRepo duplicate notesID

--- services/usernoteSvc/usernoteSvc_test.go
+++ services/usernoteSvc/usernoteSvc_test.go
@@ -9,17 +9,47 @@ import (
 )
 
 func TestNotes(t *testing.T) {
+	t.Run("When initialising NewNotesRepo, every note needs to have a different noteID", func(t *testing.T) {
+		notes := []svc.Note{
+			{
+				NoteID:  uuid.UUID{},
+				UserID:  uuid.UUID{2},
+				Title:   "annas 1st note",
+				Content: "annas 1st note content",
+			},
+			{
+				NoteID:  uuid.UUID{},
+				UserID:  uuid.UUID{2},
+				Title:   "annas 2nd note",
+				Content: "annas 2nd note content",
+			},
+		}
+		err := svc.NewNotesRepo(notes)
+		assert.ErrorContains(t, err, "newNotesRepo: duplicate noteid")
+	})
+
 	t.Run("I can get a note by its ID", func(t *testing.T) {
 		unRepo := svc.NewNotesRepo(fixtureNotes())
-		noteID := uuid.UUID{1}
-		got := unRepo.GetNoteByID(noteID)
-		want := svc.Note{
-			NoteID:  uuid.UUID{1},
-			Title:   "robs 1st note",
-			Content: "robs 1st note content",
-			UserID:  uuid.UUID{1},
+		type testCase struct {
+			noteID uuid.UUID
+			want   svc.Note
 		}
-		assert.Equal(t, want, got)
+
+		testCases := []testCase{
+			{
+				noteID: uuid.UUID{1}, want: svc.Note{
+					NoteID:  uuid.UUID{1},
+					Title:   "robs 1st note",
+					Content: "robs 1st note content",
+					UserID:  uuid.UUID{1},
+				},
+			},
+		}
+
+		for _, tc := range testCases {
+			got := unRepo.GetNoteByID(tc.noteID)
+			assert.Equal(t, tc.want, got)
+		}
 	})
 
 	t.Run("I can get all notes of a User by the userID", func(t *testing.T) {
