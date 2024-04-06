5efe17ab7531dcb2cc1f10cbf55b2203c11613ca
Kay Freyer <kay.freyer@icloud.com>
2024-04-06 10:36:04 +0200

    Red: Get notes by UserID

--- services/usernoteSvc/usernoteSvc_test.go
+++ services/usernoteSvc/usernoteSvc_test.go
@@ -9,14 +9,21 @@ import (
 )
 
 func TestGet(t *testing.T) {
-	t.Run("Get note by UserID", func(t *testing.T) {
-		want := usernoteSvc.Note{
-			UserID:  uuid.UUID{1},
-			Title:   "robs note",
-			Content: "robs note content",
+	t.Run("Get notes by UserID", func(t *testing.T) {
+		want := []usernoteSvc.Note{
+			{
+				UserID:  uuid.UUID{1},
+				Title:   "robs 1st note",
+				Content: "robs 1st note content",
+			},
+			{
+				UserID:  uuid.UUID{1},
+				Title:   "robs 2nd note",
+				Content: "robs 2nd note content",
+			},
 		}
 
-		got := usernoteSvc.GetNoteByUserID(uuid.UUID{1})
-		assert.Equal(t, want, got)
+		got := usernoteSvc.GetNotesByUserID(uuid.UUID{1})
+		assert.ElementsMatch(t, want, got)
 	})
 }
