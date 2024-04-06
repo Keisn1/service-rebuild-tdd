51e44fbef6cf823a893360825431efdd19a68173
Kay Freyer <kay.freyer@icloud.com>
2024-04-06 10:19:56 +0200

    Red:Get note by name

--- services/usernoteSvc/usernoteSvc_test.go
+++ services/usernoteSvc/usernoteSvc_test.go
@@ -1,1 +1,24 @@
 package usernoteSvc_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGet(t *testing.T) {
+	t.Run("Get robs notes", func(t *testing.T) {
+		want := struct {
+			owner    string
+			NoteName string
+			NoteText string
+		}{
+			owner:    "rob",
+			NoteName: "robs note",
+			NoteText: "robs note text",
+		}
+
+		got := GetNoteByName("rob")
+		assert.Equal(t, want, got)
+	})
+}
