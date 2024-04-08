7ca6eb401d81c4ad298d86145a4136a488bbe5ce
Kay Freyer <kay.freyer@icloud.com>
2024-04-08 18:38:12 +0200

    Green!

--- domain/note/repositories/postgres/postgres.go
+++ domain/note/repositories/postgres/postgres.go
@@ -8,13 +8,42 @@ import (
 )
 
 type NoteRepo struct {
-	conn *sql.Conn
+	db *sql.DB
 }
 
-func NewNotesRepo(conn *sql.Conn) NoteRepo {
-	return NoteRepo{conn: conn}
+func NewNotesRepo(db *sql.DB) NoteRepo {
+	return NoteRepo{db: db}
+}
+
+type noteDB struct {
+	id      uuid.UUID
+	title   string
+	content string
+	userID  uuid.UUID
 }
 
 func (nR NoteRepo) GetNotesByUserID(userID uuid.UUID) ([]note.Note, error) {
-	return nil, nil
+	getNote := `
+SELECT id, title, content, user_id FROM notes WHERE user_id=$1;
+`
+	rows, _ := nR.db.Query(getNote, userID)
+	var notes []noteDB
+	for rows.Next() {
+
+		var n noteDB
+		_ = rows.Scan(&n.id, &n.title, &n.content, &n.userID)
+		notes = append(notes, n)
+	}
+
+	var ret []note.Note
+	for _, n := range notes {
+		ret = append(ret, note.MakeNote(
+			n.id,
+			note.NewTitle(n.title),
+			note.NewContent(n.content),
+			n.userID,
+		))
+	}
+
+	return ret, nil
 }
--- domain/note/repositories/postgres/postgres_test.go
+++ domain/note/repositories/postgres/postgres_test.go
@@ -4,16 +4,20 @@ import (
 	"testing"
 
 	"database/sql"
+
 	"github.com/Keisn1/note-taking-app/domain/note"
 	"github.com/Keisn1/note-taking-app/domain/note/repositories/postgres"
 	"github.com/google/uuid"
+	_ "github.com/jackc/pgx/v5/stdlib"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestPostGres(t *testing.T) {
 	t.Run("Get notes of users by UserID", func(t *testing.T) {
-		conn := SetupPostGres(fixtureNotes())
-		notesR := postgres.NewNotesRepo(conn)
+		db := SetupPostGres(t, fixtureNotes())
+		defer db.Close()
+
+		notesR := postgres.NewNotesRepo(db)
 		type testCase struct {
 			userID uuid.UUID
 			want   []note.Note
@@ -23,15 +27,15 @@ func TestPostGres(t *testing.T) {
 			{
 				userID: uuid.UUID{1},
 				want: []note.Note{
-					note.MakeNote(uuid.UUID{}, note.NewTitle("robs 1st note"), note.NewContent("robs 1st note content"), uuid.UUID{1}),
-					note.MakeNote(uuid.UUID{}, note.NewTitle("robs 2nd note"), note.NewContent("robs 2nd note content"), uuid.UUID{1}),
+					note.MakeNote(uuid.UUID{1}, note.NewTitle("robs 1st note"), note.NewContent("robs 1st note content"), uuid.UUID{1}),
+					note.MakeNote(uuid.UUID{2}, note.NewTitle("robs 2nd note"), note.NewContent("robs 2nd note content"), uuid.UUID{1}),
 				},
 			},
 			{
 				userID: uuid.UUID{2},
 				want: []note.Note{
-					note.MakeNote(uuid.UUID{}, note.NewTitle("annas 1st note"), note.NewContent("annas 1st note content"), uuid.UUID{2}),
-					note.MakeNote(uuid.UUID{}, note.NewTitle("annas 2nd note"), note.NewContent("annas 2nd note content"), uuid.UUID{2}),
+					note.MakeNote(uuid.UUID{3}, note.NewTitle("annas 1st note"), note.NewContent("annas 1st note content"), uuid.UUID{2}),
+					note.MakeNote(uuid.UUID{4}, note.NewTitle("annas 2nd note"), note.NewContent("annas 2nd note content"), uuid.UUID{2}),
 				},
 			},
 		}
@@ -54,6 +58,50 @@ func fixtureNotes() []note.Note {
 	}
 }
 
-func SetupPostGres(notes []note.Note) *sql.Conn {
-	return nil
+func SetupPostGres(t *testing.T, notes []note.Note) *sql.DB {
+	url := "postgres://simba:mufassa@localhost:5432/test_note_taking_app"
+	db, err := sql.Open("pgx", url)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// dropDB := `DROP DATABASE IF EXISTS test_note_taking_app;`
+	// createDB := `CREATE DATABASE test_note_taking_app;`
+	dropNoteTable := `DROP TABLE IF EXISTS notes;`
+	createNoteTable := `
+CREATE TABLE notes(
+id UUID PRIMARY KEY,
+title TEXT,
+content TEXT,
+user_id UUID NOT NULL
+)
+`
+	_, err = db.Exec(dropNoteTable)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	_, err = db.Exec(createNoteTable)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	insertRow := `INSERT INTO notes (id, title, content, user_id) VALUES ($1, $2, $3, $4)`
+	for _, n := range notes {
+		_, err = db.Exec(
+			insertRow,
+			n.GetID(),
+			n.GetTitle().Get(),
+			n.GetContent().Get(),
+			n.GetUserID(),
+		)
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	return db
 }
+
+//TODO: need to have a timeout context for getting the database connection
