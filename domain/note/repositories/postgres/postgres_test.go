eda9f5b7d9a932d4c9827db38812cc39f663e46d
Kay Freyer <kay.freyer@icloud.com>
2024-04-09 13:21:40 +0200

    refactor test: extract to stubs and setup

--- domain/note/repositories/postgres/postgres_test.go
+++ domain/note/repositories/postgres/postgres_test.go
@@ -26,50 +26,6 @@ func TestMain(m *testing.M) {
 	os.Exit(exitCode)
 }
 
-func run(m *testing.M) int {
-	var (
-		dropDB   = fmt.Sprintf(`DROP DATABASE IF EXISTS %s;`, testDBName)
-		createDB = fmt.Sprintf(`CREATE DATABASE %s;`, testDBName)
-	)
-
-	dsn := fmt.Sprintf("host=localhost port=5432 user=%s password=%s sslmode=disable", testUser, testPassword)
-	postgresDB, err := sql.Open("pgx", dsn)
-	if err != nil {
-		panic(err)
-	}
-	defer postgresDB.Close()
-
-	_, err = postgresDB.Exec(dropDB)
-	if err != nil {
-		panic(err)
-	}
-
-	_, err = postgresDB.Exec(createDB)
-	if err != nil {
-		panic(err)
-	}
-
-	defer func() {
-		_, err = postgresDB.Exec(dropDB)
-		if err != nil {
-			panic(fmt.Errorf("postgresDB.Exec() err = %s", err))
-		}
-	}()
-
-	return m.Run()
-
-}
-
-type SQLDB struct{}
-
-func (s *SQLDB) Query(query string, args ...interface{}) (*sql.Rows, error) {
-	return nil, errors.New("DBError")
-}
-
-func (s *SQLDB) QueryRow(query string, args ...any) (row *sql.Row) {
-	return
-}
-
 func TestNotesRepo_GetNoteByID(t *testing.T) {
 	testDB, deleteTable := SetupNotesTable(t, fixtureNotes())
 	defer testDB.Close()
@@ -158,59 +114,4 @@ func TestNotesRepo_GetNotesByUserID(t *testing.T) {
 		assert.EqualError(t, err, wantErr.Error())
 	})
 
-}
-
-func SetupNotesTable(t *testing.T, notes []note.Note) (*sql.DB, func()) {
-	var (
-		createNoteTable = `CREATE TABLE notes(
-							id UUID PRIMARY KEY,
-							title TEXT,
-							content TEXT,
-							user_id UUID NOT NULL)`
-		dropNotesTable = `DROP TABLE notes`
-	)
-
-	dsn := fmt.Sprintf("host=localhost port=5432 user=%s password=%s sslmode=disable dbname=%s ", testUser, testPassword, testDBName)
-	testDB, err := sql.Open("pgx", dsn)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	_, err = testDB.Exec(createNoteTable)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	insertRow := `INSERT INTO notes (id, title, content, user_id) VALUES ($1, $2, $3, $4)`
-	for _, n := range notes {
-		_, err = testDB.Exec(
-			insertRow,
-			n.GetID(),
-			n.GetTitle().Get(),
-			n.GetContent().Get(),
-			n.GetUserID(),
-		)
-		if err != nil {
-			t.Fatal(err)
-		}
-	}
-
-	deleteTable := func() {
-		_, err := testDB.Exec(dropNotesTable)
-		if err != nil {
-			t.Fatal(err)
-		}
-		testDB.Close()
-	}
-
-	return testDB, deleteTable
-}
-
-func fixtureNotes() []note.Note {
-	return []note.Note{
-		note.MakeNote(uuid.UUID{1}, note.NewTitle("robs 1st note"), note.NewContent("robs 1st note content"), uuid.UUID{1}),
-		note.MakeNote(uuid.UUID{2}, note.NewTitle("robs 2nd note"), note.NewContent("robs 2nd note content"), uuid.UUID{1}),
-		note.MakeNote(uuid.UUID{3}, note.NewTitle("annas 1st note"), note.NewContent("annas 1st note content"), uuid.UUID{2}),
-		note.MakeNote(uuid.UUID{4}, note.NewTitle("annas 2nd note"), note.NewContent("annas 2nd note content"), uuid.UUID{2}),
-	}
 }
---
+++ domain/note/repositories/postgres/stubs_test.go
@@ -0,0 +1,16 @@
+package postgres_test
+
+import (
+	"database/sql"
+	"errors"
+)
+
+type SQLDB struct{}
+
+func (s *SQLDB) Query(query string, args ...interface{}) (*sql.Rows, error) {
+	return nil, errors.New("DBError")
+}
+
+func (s *SQLDB) QueryRow(query string, args ...any) (row *sql.Row) {
+	return
+}
